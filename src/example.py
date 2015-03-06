959068387481cb0acefdcc2f1e15cfc1be85a21d
WebTogz <antonin.carette@gmail.com>
2015-03-06 18:52:21 +0100

    PACMAN STYLE

--- src/example.py
+++ src/example.py
@@ -1,9 +1,9 @@
 import ProgressBar
 import time
 
-def testPB(valmax, maxbar, title):
+def testPB(valmax, maxbar, title, pacman_style = False):
     i = 1
-    barTest = ProgressBar.ProgressBar(valmax, maxbar, title)
+    barTest = ProgressBar.ProgressBar(valmax, maxbar, title, pacman_style)
     for _ in range(valmax):
         barTest.update(i)
         time.sleep(0.50)
