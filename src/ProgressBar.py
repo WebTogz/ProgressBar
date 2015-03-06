ffe9e820470565733689db4cbff604e26e42aee0
WebTogz <antonin.carette@gmail.com>
2015-03-06 18:52:10 +0100

    PACMAN STYLE

--- src/ProgressBar.py
+++ src/ProgressBar.py
@@ -1,12 +1,13 @@
 import sys
 import os
+import math
 
 class ProgressBar:
     """
     Progress bar object
     This object is caracterized by a maximal value, the maximal value of the bar, and the title of the bar
     """
-    def __init__ (self, valmax, maxbar, title):
+    def __init__ (self, valmax, maxbar, title, pacman_style = False):
         if valmax <= 0:  
             valmax = 1
         if maxbar <= 0:
@@ -18,7 +19,16 @@ class ProgressBar:
         self.valmax = valmax
         self.maxbar = maxbar
         self.title  = title
-    
+        if pacman_style:
+            self.progress_before_tip = '-'
+            self.progress_after_tip = 'o'
+            self.pacman_icon_style = True
+        else:
+            self.progress_before_tip = '#'
+            self.progress_after_tip = ' '
+            self.progress_tip = '>'
+            self.pacman_icon_style = None
+
     def update(self, val):
         """
         Method to update the ProgressBar object
@@ -29,5 +39,14 @@ class ProgressBar:
         perc  = round((float(val) / float(self.valmax)) * 100)
         scale = 100.0 / float(self.maxbar)
         bar   = int(perc / scale)
+
+        mul_coef = math.floor((self.maxbar - bar)/len(self.progress_after_tip))
+
+        if self.pacman_icon_style != None:
+            if self.pacman_icon_style:
+                self.progress_tip = '\033[1;33;40mC\033[0m'
+            if not self.pacman_icon_style:
+                self.progress_tip = '\033[1;33;40mc\033[0m'
+            self.pacman_icon_style = not self.pacman_icon_style
   
-        print("{0} [{1}>{2}] {3}%".format(self.title, '=' * bar, ' ' * (self.maxbar - bar), perc), end="\r")
+        print("{0} [{1}{2}{3}] {4}%".format(self.title, self.progress_before_tip * bar, self.progress_tip, self.progress_after_tip * mul_coef, perc), end="\r")
